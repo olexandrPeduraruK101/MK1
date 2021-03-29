cb24b0fab799c2b4492b17405b20dac165468b9b
olexandrPeduraruK101 <78420095+olexandrPeduraruK101@users.noreply.github.com>
2021-03-29 21:37:16 +0300

    Update main.c

--- mk1/mk1 2/mk1 2/main.c
+++ mk1/mk1 2/mk1 2/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
+#include <locale.h>//підключення бібліотек
 
-int main() {
+int main() {//запуск програми
 
 	unsigned int katet1; unsigned int katet2;
-	float gipotenuza; float s;
+	float gipotenuza; float s; // введення змінних
 
 	setlocale(LC_CTYPE, "ru");
-	printf("Вед1ть значення першого катета: ");
-		scanf("%d", &katet1);
-		printf("Вед1ть значення другого катета: ");
-		scanf("%d", &katet2);
-		gipotenuza = (float)sqrt(pow(katet1, 2) + pow(katet2, 2));
-		s = (float) 0.5 * katet1 * katet2;
-		printf("\n\t гипотенуза \t площа\n");
-		printf("\n\t %.3f \t         %.3f \n", gipotenuza, s);
+	printf("Вед1ть значення першого катета: ");//ввиведення даних
+		scanf("%d", &katet1);//введення даних
+		printf("Вед1ть значення другого катета: ");//ввиведення даних
+		scanf("%d", &katet2);//введення даних
+		gipotenuza = (float)sqrt(pow(katet1, 2) + pow(katet2, 2));// formula pifagora
+		s = (float) 0.5 * katet1 * katet2;//ploscha trikutnika
+		printf("\n\t гипотенуза \t площа\n");//ввиведення даних
+		printf("\n\t %.3f \t         %.3f \n", gipotenuza, s);//ввиведення даних
 		
-	return 0;
+	return 0; //кінець програми
 }
